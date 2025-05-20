f456f07afc2d48f323311f6aa490f5deb000c9a8
Alexander Brefeld <alexander.brefeld@protonmail.com>
2025-05-20 12:35:31 -0600

    init commit

---
+++ resources/setupdb.sql
@@ -0,0 +1,3 @@
+alter role todo_manager set client_encoding to 'utf8';
+alter role todo_manager set default_transaction_isolation to 'read committed';
+alter role todo_manager set timezone 'US/Mountain';
